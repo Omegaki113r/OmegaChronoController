d148a8072bb988acfeb257c843244a3570ba168f
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2025-01-28 02:05:12 +0530

    initial freertos commit

--- inc/OmegaChronoController/ChronoController.hpp
+++ inc/OmegaChronoController/ChronoController.hpp
@@ -10,7 +10,7 @@
  * File Created: Monday, 27th January 2025 8:21:55 am
  * Author: Omegaki113r (omegaki113r@gmail.com)
  * -----
- * Last Modified: Monday, 27th January 2025 8:48:16 am
+ * Last Modified: Tuesday, 28th January 2025 1:55:11 am
  * Modified By: Omegaki113r (omegaki113r@gmail.com)
  * -----
  * Copyright 2025 - 2025 0m3g4ki113r, Xtronic
@@ -21,6 +21,14 @@
  */
 
 #pragma once
+
+#include <cstring>
+#include <functional>
+#include <utility>
+#include <variant>
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/timers.h>
 
 #include "OmegaUtilityDriver/UtilityDriver.hpp"
 
@@ -38,13 +46,70 @@ namespace Omega
             eCOUNTUP
         };
 
+        enum class State
+        {
+            eIDLE,
+            eCREATED,
+            eSTARTED,
+            ePAUSED,
+            eSTOPPED,
+            eDELETED,
+        };
+
+        struct Duration
+        {
+            u64 us;
+            u32 ms;
+            u32 s;
+            u16 m;
+            u16 h;
+
+            Duration(u64 in_duration_us);
+            bool operator==(const Duration &other) const
+            {
+                return h == other.h && m == other.m && s == other.s && ms == other.ms && us == other.us;
+            }
+        };
+
         class FreeRTOS
         {
         public:
-            FreeRTOS(Type in_type = Type::eSINGLE_SHOT);
+            FreeRTOS(Type in_type = Type::eSINGLE_SHOT, Duration in_duration = {0}, Duration in_update_period = {0}, Duration in_delay = {0})
+                : type(in_type), duration(in_duration), update_period(in_update_period), delay(in_delay) {}
             ~FreeRTOS();
 
+            inline void set_type(Type in_type) noexcept { type = in_type; }
+            inline void set_duration(Duration in_duration) noexcept { duration = in_duration; }
+            inline void set_update_period(Duration in_update_period) noexcept { update_period = in_update_period; }
+            inline void set_delay(Duration in_delay) noexcept { delay = in_delay; }
+            inline void add_on_start_callback(std::function<void(void)> in_callback) noexcept { on_start = in_callback; }
+            inline void add_on_update_callback(std::function<void(void)> in_callback) noexcept { on_update = in_callback; }
+            inline void add_on_end_callback(std::function<void(void)> in_callback) noexcept { on_end = in_callback; }
+            OmegaStatus start() noexcept;
+            OmegaStatus pause() noexcept;
+            OmegaStatus resume() noexcept;
+            OmegaStatus stop() noexcept;
+
+            inline Type get_type() const noexcept { return type; }
+            inline Duration get_duration() const noexcept { return duration; }
+            inline Duration get_update_period() const noexcept { return update_period; }
+            inline Duration get_delay() const noexcept { return delay; }
+
+            FreeRTOS(FreeRTOS &other) = delete;
+            FreeRTOS &operator=(FreeRTOS &) = delete;
+            FreeRTOS(FreeRTOS &&) = delete;
+            FreeRTOS &operator=(FreeRTOS &&) = delete;
+
         private:
+            Type type;
+            Duration duration;
+            Duration update_period;
+            Duration delay;
+            std::function<void(void)> on_start;
+            std::function<void(void)> on_update;
+            std::function<void(void)> on_end;
+            State state{State::eIDLE};
+            TimerHandle_t handle;
         };
     } // namespace Chrono
 } // namespace Omega
--- src/ChronoController.cpp
+++ src/ChronoController.cpp
@@ -10,7 +10,7 @@
  * File Created: Monday, 27th January 2025 8:22:29 am
  * Author: Omegaki113r (omegaki113r@gmail.com)
  * -----
- * Last Modified: Monday, 27th January 2025 8:48:49 am
+ * Last Modified: Tuesday, 28th January 2025 2:04:36 am
  * Modified By: Omegaki113r (omegaki113r@gmail.com)
  * -----
  * Copyright 2025 - 2025 0m3g4ki113r, Xtronic
@@ -20,31 +20,88 @@
  * ----------	---	---------------------------------------------------------
  */
 
+#include <freertos/FreeRTOS.h>
+#include <freertos/timers.h>
+
 #include "OmegaChronoController/ChronoController.hpp"
 #include "OmegaUtilityDriver/UtilityDriver.hpp"
+
+#include <sdkconfig.h>
+#if CONFIG_OMEGA_CHRONO_CONTROLLER_DEBUG
+#define LOGD(format, ...) OMEGA_LOGD(format, ##__VA_ARGS__)
+#define HEX_LOGD(buffer, length) OMEGA_HEX_LOGD(buffer, length)
+#else
+#define LOGD(format, ...)
+#define HEX_LOGD(buffer, length)
+#endif
+#if CONFIG_OMEGA_CHRONO_CONTROLLER_INFO
+#define LOGI(format, ...) OMEGA_LOGI(format, ##__VA_ARGS__)
+#define HEX_LOGI(buffer, length) OMEGA_HEX_LOGI(buffer, length)
+#else
+#define LOGI(format, ...)
+#define HEX_LOGI(buffer, length)
+#endif
+
+#if CONFIG_OMEGA_CHRONO_CONTROLLER_WARN
+#define LOGW(format, ...) OMEGA_LOGW(format, ##__VA_ARGS__)
+#define HEX_LOGW(buffer, length) OMEGA_HEX_LOGW(buffer, length)
+#else
+#define LOGW(format, ...)
+#define HEX_LOGW(buffer, length)
+#endif
+
+#if CONFIG_OMEGA_CHRONO_CONTROLLER_ERROR
+#define LOGE(format, ...) OMEGA_LOGE(format, ##__VA_ARGS__)
+#define HEX_LOGE(buffer, length) OMEGA_HEX_LOGE(buffer, length)
+#else
+#define LOGE(format, ...)
+#define HEX_LOGE(buffer, length)
+#endif
 
 #ifdef __cplusplus
 namespace Omega
 {
     namespace Chrono
     {
-        FreeRTOS::FreeRTOS(Type in_type)
-        {
-            switch (in_type)
-            {
-            case Type::eCOUNTDOWN:
-            {
-                break;
-            }
-            default:
-            {
-                break;
-            }
-            }
-        }
+        Duration::Duration(u64 in_duration_us) : us(in_duration_us), ms(us / 1000), s(ms / 1000), m(s / 60), h(m / 60) { LOGD("%lld %ld %ld %d %d", us, ms, s, m, h); }
+
         FreeRTOS::~FreeRTOS()
         {
+            // if (nullptr == handle)
+            //     return;
+            // xTimerDelete(handle, portMAX_DELAY);
+            LOGD("Cleaned");
         }
+
+        OmegaStatus FreeRTOS::start() noexcept
+        {
+            const auto x = [](TimerHandle_t handle)
+            { LOGE("Critical error"); };
+            if (Duration(0) == duration)
+            {
+                LOGE("Invalid duration: %lld", duration.us);
+                return eFAILED;
+            }
+            handle = xTimerCreate("timer", pdMS_TO_TICKS(duration.ms), pdFALSE, nullptr, x);
+            if (nullptr == handle)
+            {
+                LOGE("xTimerCreate failed");
+                return eFAILED;
+            }
+            delay_ms(delay.ms);
+            if (const auto err = xTimerStart(handle, portMAX_DELAY); pdPASS != err)
+            {
+                LOGE("xTimerStart failed");
+                return eFAILED;
+            }
+            if (nullptr != on_start)
+                on_start();
+            return eSUCCESS;
+        }
+
+        OmegaStatus FreeRTOS::pause() noexcept { return eSUCCESS; }
+        OmegaStatus FreeRTOS::resume() noexcept { return eSUCCESS; }
+        OmegaStatus FreeRTOS::stop() noexcept { return eSUCCESS; }
     } // namespace Chrono
 } // namespace Omega
 #else
