d36b16e01b4e453cfd1352e1fe4036734147cb9e
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2025-02-08 18:45:03 +0530

    fixing example code

--- examples/countdown-ESP32xxHiRes/main/main.cpp
+++ examples/countdown-ESP32xxHiRes/main/main.cpp
@@ -10,7 +10,7 @@
  * File Created: Sunday, 19th January 2025 12:42:20 am
  * Author: Omegaki113r (omegaki113r@gmail.com)
  * -----
- * Last Modified: Saturday, 8th February 2025 6:27:47 pm
+ * Last Modified: Saturday, 8th February 2025 6:44:54 pm
  * Modified By: Omegaki113r (omegaki113r@gmail.com)
  * -----
  * Copyright 2025 - 2025 0m3g4ki113r, Xtronic
@@ -73,12 +73,6 @@ extern "C" void app_main(void)
         if (const auto state = countdown.stop(); eSUCCESS != state)
         {
             OMEGA_LOGE("countdown stop failed");
-            return;
-        }
-        delay_s(5);
-        if (const auto state = countdown.resume(); eSUCCESS != state)
-        {
-            OMEGA_LOGE("countdown resume failed");
             return;
         }
     }
